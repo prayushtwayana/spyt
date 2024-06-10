2ab3e7ae82adae252255a4743824643fd25bfaae
Prayush Twayana <prayush.twayana@gmail.com>
2024-06-10 18:01:21 +0545

    integrated OOP

--- src/main.py
+++ src/main.py
@@ -39,37 +39,65 @@ def download_tracks(file_name):
 
 
 def main():
+    # token = spotify.get_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+    # link = spotify.get_link()
+    # link_type = spotify.identify_link(link)
+    #
+    # if link_type == None: sys.exit("Invalid Input")
+    # if not link_type in ['Album', 'Playlist']:
+    #     sys.exit(f"The link is a Spotify {identify_link(link)} link.")
+    #
+    # print(f"The link is a Spotify {link_type} link.")
+    #
+    # match link_type:
+    #     case 'Playlist':
+    #         playlist_id = spotify.get_playlist_id(link)
+    #         playlist_name = spotify.get_playlist_name(token, playlist_id)
+    #         tracks = spotify.get_playlist_tracks(token, playlist_id)
+    #         artists = spotify.get_playlist_artist(token, playlist_id)
+    #         search_list = spotify.get_optimized_search(tracks, artists)
+    #         file_name = playlist_name + '.csv'
+    #
+    #     case 'Album':
+    #         album_id = spotify.get_album_id(link)
+    #         album_name = spotify.get_album_name(token, album_id)
+    #         tracks = spotify.get_album_tracks(token, album_id)
+    #         artists = spotify.get_album_artist(token, album_id)
+    #         search_list = spotify.get_optimized_search(tracks, artists)
+    #         file_name = album_name + '.csv'
+    #
+    # youtube_key = youtube.build_youtube_key(YOUTUBE_API_KEY)
+    # for search_query in search_list:
+    #     request = youtube_key.search().list(
+    #         part = 'snippet',
+    #         type = 'video',
+    #         maxResults = 1,
+    #         order ='relevance',
+    #         q = search_query
+    #     )
+    #
+    #     response = request.execute()
+    #     links = youtube.get_result(response)
+    #     print(links)
+    #     youtube.save_results_to_file(links, file_name)
+    #
+    # download_tracks(file_name)
 
 
-    token = spotify.get_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
-    link = spotify.get_link()
-    link_type = spotify.identify_link(link)
 
-    if link_type == None: sys.exit("Invalid Input")
-    if not link_type in ['Album', 'Playlist']:
-        sys.exit(f"The link is a Spotify {identify_link(link)} link.")
-
-    print(f"The link is a Spotify {link_type} link.")
-
-    match link_type:
-        case 'Playlist':
-            playlist_id = spotify.get_playlist_id(link)
-            playlist_name = spotify.get_playlist_name(token, playlist_id)
-            tracks = spotify.get_playlist_tracks(token, playlist_id)
-            artists = spotify.get_playlist_artist(token, playlist_id)
-            search_list = spotify.get_optimized_search(tracks, artists)
-            file_name = playlist_name + '.csv'
-
-        case 'Album':
-            album_id = spotify.get_album_id(link)
-            album_name = spotify.get_album_name(token, album_id)
-            tracks = spotify.get_album_tracks(token, album_id)
-            artists = spotify.get_album_artist(token, album_id)
-            search_list = spotify.get_optimized_search(tracks, artists)
-            file_name = album_name + '.csv'
+    link = input("Enter a link: ")
+    s = spotify.Spotify(link)
+    token = s.get_auth_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+    header = s.get_auth_header()
+    id = s.get_link_id()
+    entity_name = s.get_name()
+    tracks = s.get_tracks()
+    artists = s.get_artists()
+    search = s.optimized_search(artists)
+    file_name = entity_name + '.csv'
 
     youtube_key = youtube.build_youtube_key(YOUTUBE_API_KEY)
-    for search_query in search_list:
+    for search_query in search:
         request = youtube_key.search().list(
             part = 'snippet',
             type = 'video',
--- src/spotify/spotify.py
+++ src/spotify/spotify.py
@@ -1,268 +1,213 @@
 import json
 import sys
-import re
 import base64
+import re
 from requests import post, get
-from pprint import pprint
 
+class Spotify:
+    def __init__(self, link):
+        self._link = link.strip()
+        self._type = None
+        self._token = None
+        self._auth_header = None
+        self._id = None
+        self._entity_name = None
+        self._tracks = None
 
-def get_token(CLIENT_ID: str, CLIENT_SECRET: str) -> str:
-    """
-    This function requests access token to the API service provider and returns the access token.
-    This access token is crucial for making any requests to the API.
-    For each request to the API the access token is necessary.
+        if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/track/([a-zA-Z\d]{22})\?si=([a-zA-Z\d]{16})$", self._link):
+            self._type = "Track"
 
-    """
-    auth_string = CLIENT_ID + ":" + CLIENT_SECRET
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+        if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/episode/([a-zA-Z\d]{22})\?si=([a-zA-Z\d]{16})$", self._link):
+            self._type = "Episode"
 
-    url = "https://accounts.spotify.com/api/token"
+        if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/artist/([a-zA-Z\d]{22})\?si=([a-zA-Z\d_]{22})$", self._link):
+            self._type = "Artist"
 
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
+        if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/album/([a-zA-Z\d]{22})\?si=([a-zA-Z\d]{22})$", self._link):
+            self._type = "Album"
 
-    data = {"grant_type": "client_credentials"}
+        if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/show/([a-zA-Z\d]{22})\?si=([a-zA-Z\d]{16})$", self._link):
+            self._type = "Show"
 
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
+        if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/playlist/([a-zA-Z\d]{22})\?si=([a-zA-Z\d]{16})$", self._link):
+            self._type = "Playlist"
 
-    return token
+        self._check_type()
 
 
-def get_auth_header(token: str) -> dict:
-    """
-    The authenticated header string is a unique header that is required for making requests to the API
-    (insert more info about auth_header here)
+    def _check_type(self):
+        """
+        This function checks whether the type of link is eligible for spyt (for now only Playlist and ALbums are supported)
 
-    parameters:
-    - token (str) : the token from earlier
+        """
+        if not self._type in ['Album', 'Playlist']:
+            sys.exit("Invalid Input! (Enter a Spotify Album or Playlist Link)")
+        else:
+            print(f"The link is a Spotify {self._type} link.")
 
-    """
 
-    return {"Authorization": "Bearer " + token}
+    def get_auth_token(self, CLIENT_ID, CLIENT_SECRET):
+        """
+        This function returns an authentication token, which is used to generate the authentication header
 
+        """
+        auth_string = CLIENT_ID + ":" + CLIENT_SECRET
+        auth_bytes = auth_string.encode("utf-8")
+        auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
-def get_link() -> str:
-    """
-    This function prompts the user for a spotify playlist link
+        url = "https://accounts.spotify.com/api/token"
 
-    """
-    link = input("Enter a spotify playlist or album link: ")
-    return link
+        headers = {
+            "Authorization": "Basic " + auth_base64,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
 
+        data = {"grant_type": "client_credentials"}
 
-def identify_link(link: str):
-    """
-    This function informs the user about the type of link they have provided should the link be not a Spotify Playlist Link
+        result = post(url, headers=headers, data=data)
+        json_result = json.loads(result.content)
+        self._token = json_result["access_token"]
 
-    parameters:
-    - link (str) : the link obtained from get_playlist_link function
+        return self._token
 
-    """
-    link = link.strip()
 
-    if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/track/([a-zA-Z\d]{22})\?si=([a-zA-Z\d]{16})$", link):
-        return "Track"
+    def get_auth_header(self):
+        """
+        This function returns an authentication header, which is required for API interaction
 
-    if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/episode/([a-zA-Z\d]{22})\?si=([a-zA-Z\d]{16})$", link):
-        return "Episode"
+        """
+        if (self._token):
+            self._auth_header = {"Authorization": "Bearer " + self._token}
+        else:
+            sys.exit("Please get the authentication token first from `get_auth_token()`")
 
-    if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/artist/([a-zA-Z\d]{22})\?si=([a-zA-Z\d_]{22})$", link):
-        return "Artist"
 
-    if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/album/([a-zA-Z\d]{22})\?si=([a-zA-Z\d]{22})$", link):
-        return "Album"
+    def get_link_id(self):
+        """
+        This function returns the id of a spotify link
 
-    if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/show/([a-zA-Z\d]{22})\?si=([a-zA-Z\d]{16})$", link):
-        return "Show"
+        """
+        link_split = self._link.split('?')
+        link_split = link_split[0].rsplit('/', )
+        self._id = link_split[4]
 
-    if re.search(r".*(?:https?://)?(?:open\.)?spotify\.com/playlist/([a-zA-Z\d]{22})\?si=([a-zA-Z\d]{16})$", link):
-        return "Playlist"
+        return self._id
 
 
-def get_album_id(album_link: str) -> str:
-    album_split = album_link.split('?')
-    album_split = album_split[0].rsplit('/', )
-    album_id = album_split[4]
+    def get_name(self):
+        """
+        This function returns the name of the album or playlist name
 
-    return album_id
+        """
+        if self._type == "Album":
+            url = f"https://api.spotify.com/v1/albums/{self._id}"
+            result = get(url, headers=self._auth_header)
 
+            self._entity_name = json.loads(result.content)['name']
 
-def get_album_name(token:str, album_id:str) -> str:
-    url = f"https://api.spotify.com/v1/albums/{album_id}"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
+        if self._type == "Playlist":
+            url = f"https://api.spotify.com/v1/playlists/{self._id}"
+            result = get(url, headers=self._auth_header)
 
-    album_name = json.loads(result.content)['name']
+            self._entity_name = json.loads(result.content)['name']
 
-    return album_name
+        return self._entity_name
 
 
-def get_album_tracks(token, album_id):
-    url = f"https://api.spotify.com/v1/albums/{album_id}/tracks"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
+    def get_tracks(self):
+        """
+        This function returns the tracks in an album or a playlist
 
-    json_result = json.loads(result.content)["items"]
-    track = [item['name'] for item in json_result]
+        """
+        if self._type == "Album":
+            url = f"https://api.spotify.com/v1/albums/{self._id}/tracks"
+            result = get(url, headers=self._auth_header)
 
-    return track
+            json_result = json.loads(result.content)["items"]
+            self._tracks = [item['name'] for item in json_result]
 
+        if self._type == "Playlist":
+            url = f"https://api.spotify.com/v1/playlists/{self._id}/tracks"
+            result = get(url, headers=self._auth_header)
 
-def get_album_artist(token, album_id):
-    url = f"https://api.spotify.com/v1/albums/{album_id}/tracks"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
+            json_result = json.loads(result.content)["items"]
+            self._tracks = [item['track']['name'] for item in json_result]
 
-    json_result = json.loads(result.content)["items"]
-    artists_list = [item['artists'] for item in json_result]
+        return self._tracks
 
-    artists_dict = []
 
-    for inner_list in artists_list:
-        count = 0
-        for item in inner_list:
-            count = count + 1
-            if not count > 1:
-                artists_dict.append(item)
+    def get_artists(self):
+        """
+        This function returns the name of one artist from a track
+        (there could be multiple artists in a track but since the track is Search engine optimized in YouTube, we only need the name of one artist to make a search)
 
-    artists = [item['name'] for item in artists_dict]
+        """
+        if self._type == "Playlist":
+            url = f"https://api.spotify.com/v1/playlists/{self._id}/tracks"
+            result = get(url, headers=self._auth_header)
 
-    return artists
+            json_result = json.loads(result.content)["items"]
+            artists_list = [item['track']['artists'] for item in json_result]
 
+            artists_dict = []
 
-def get_playlist_id(playlist_link: str) -> str:
-    """
-    This function takes a playlist link and returns the playlist id (in API requests, all we care about is the object id)
-    Every request is done referencing the id
+            for inner_list in artists_list:
+                count = 0
+                for item in inner_list:
+                    count = count + 1
+                    if not count > 1:
+                        artists_dict.append(item)
 
-    parameters:
-    - playlist_link (str) - provided by the user
+            artists = [item['name'] for item in artists_dict]
 
-    """
-    playlist_split = playlist_link.split('?')
-    playlist_split = playlist_split[0].rsplit('/', )
-    playlist_id = playlist_split[4]
+        if self._type == "Album":
+            url = f"https://api.spotify.com/v1/albums/{self._id}/tracks"
+            result = get(url, headers=self._auth_header)
 
-    return playlist_id
+            json_result = json.loads(result.content)["items"]
+            artists_list = [item['artists'] for item in json_result]
 
+            artists_dict = []
 
-def get_playlist_name(token:str, playlist_id:str) -> str:
-    """
-    This function returns the name of the playlist
-    This name will be used as the name of the file when the results are stored locally
+            for inner_list in artists_list:
+                count = 0
+                for item in inner_list:
+                    count = count + 1
+                    if not count > 1:
+                        artists_dict.append(item)
 
-    parameters:
-    - token (str) : token from earlier
-    - playlist_id (str) : id of the playlist extracted from playlist link 
+            artists = [item['name'] for item in artists_dict]
 
-    """
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
+        return artists
 
-    playlist_name = json.loads(result.content)['name']
 
-    return playlist_name
+    def optimized_search(self, artists):
+        """
+        This function returns a list with the search_query for YouTube
+        It takes the corresponding elements of each list(artist and track) and joins them together to form a string for each element in the list
+        The string so formed will be our search query itself
 
+        parameters:
+        - artist (list) : list of all the artists(one artist per song) in the playlist
+        """
+        for x in range(len(self._tracks)):
+            self._tracks[x] = self._tracks[x] + ' ' + artists[x]
 
-def get_playlist_tracks(token: str, playlist_id: str) -> list:
-    """
-    This function returns the track names of the playlist
-
-    parameters:
-    - token (str) : token from earlier
-    - playlist_id (str) : id of the playlist extracted from playlist link
-
-    """
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-
-    json_result = json.loads(result.content)["items"]
-    track = [item['track']['name'] for item in json_result]
-
-    return track
-
-
-def get_playlist_artist(token: str, playlist_id: str) -> list:
-    """
-    This function returns the name of one artist from a track
-    (there could be multiple artists in a track but since the track is Search engine optimized in YouTube, we only need the name of one artist to make a search)
-
-    parameters:
-    - token (str) : token from earlier
-    - playlist_id (str) : id of the playlist extracted from playlist link
-
-    """
-    url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-
-    json_result = json.loads(result.content)["items"]
-    artists_list = [item['track']['artists'] for item in json_result]
-
-    artists_dict = []
-
-    for inner_list in artists_list:
-        count = 0
-        for item in inner_list:
-            count = count + 1
-            if not count > 1:
-                artists_dict.append(item)
-
-    artists = [item['name'] for item in artists_dict]
-
-    return artists_dict
-
-
-def get_optimized_search(tracks: list, artists: list) -> list:
-    """
-    This function returns a list with the search_query for YouTube
-    It takes the corresponding elements of each list(artist and track) and joins them together to form a string for each element in the list
-    The string so formed will be our search query itself
-
-    parameters:
-    - tracks (list) : list of all the songs in the playlist
-    - artist (list) : list of all the artists(one artist per song) in the playlist
-    """
-    for x in range(len(tracks)):
-        tracks[x] = tracks[x] + ' ' + artists[x]
-
-    return tracks
+        return self._tracks
 
 
 def main(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET):
-    token = get_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
-    link = get_link()
-    link_type = identify_link(link)
+    link = input("Enter a link: ")
+    s = Spotify(link)
+    token = s.get_auth_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+    header = s.get_auth_header()
+    id = s.get_link_id()
+    entity_name = s.get_name()
+    tracks = s.get_tracks()
+    artists = s.get_artists()
+    search = s.optimized_search(artists)
 
-    if link_type == None: sys.exit("Invalid Input")
-    if not link_type in ['Album', 'Playlist']:
-        sys.exit(f"The link is a Spotify {identify_link(link)} link.")
-
-    print(f"The link is a Spotify {link_type} link.")
-
-    match link_type:
-        case 'Playlist':
-            playlist_id = get_playlist_id(link)
-            playlist_name = get_playlist_name(token, playlist_id)
-            tracks = get_playlist_tracks(token, playlist_id)
-            artists = get_playlist_artist(token, playlist_id)
-
-        case 'Album':
-            album_id = get_album_id(link)
-            album_name = get_album_name(token, album_id)
-            tracks = get_album_tracks(token, album_id)
-            artists = get_album_artist(token, album_id)
-
-    optimized_search = get_optimized_search(tracks, artists)
-
-    print(optimized_search)
+    print(search)
 
 
 if __name__ == "__main__":
